3b40a8068d71235da47713161489e881d91ad259
klauha <claudiaalvaro17@gmail.com>
2024-02-04 13:43:41 +0100

    añadido readme

--- js/main.js
+++ js/main.js
@@ -4,43 +4,63 @@ let screen = document.getElementById("screen")
 const buttonOnOff = document.getElementById('on-off')
 const btnNetflix = document.getElementById("buttontNetflix")
 const btnYoutube = document.getElementById("buttonYoutube")
+const btnZptop = document.getElementById("zpTop")
 let tvOnOf = false;
 
-let zapping = 2
+let zapping = 1
 
 buttonOnOff.addEventListener('click', () => {
     if (tvOnOf) {
-        tvOnOf = false 
+        tvOnOf = false
         screen.classList.remove(screen.classList[screen.classList.length - 1])
         screen.classList.add("basicScreen")
         buttonOnOff.style.backgroundColor = "black"
+
+        // todo guardar en zapping el canal donde estamoss    
     } else {
         tvOnOf = true
         screen.classList.remove(screen.classList[screen.classList.length - 1])
-        screen.classList.add("canal2")
+        screen.classList.add("startingScreen")
         buttonOnOff.style.backgroundColor = "green"
+
+        // cambia la imagen despues de un tiempo
+        setTimeout(function () {
+            screen.classList.remove("startingScreen")
+            screen.classList.add("canal1")
+        }, 3000)
+
     }
 })
 
-btnNetflix.addEventListener('click', ()=> {
+btnNetflix.addEventListener('click', () => {
     if (tvOnOf) {
         screen.classList.remove(screen.classList[screen.classList.length - 1])
         screen.classList.add("netflix")
     }
 })
 
-btnYoutube.addEventListener('click',() => {
+btnYoutube.addEventListener('click', () => {
     if (tvOnOf) {
-    screen.classList.remove(screen.classList[screen.classList.lenght -1])
-    screen.classList.add("youtube")
+        screen.classList.remove(screen.classList[screen.classList.lenght - 1])
+        screen.classList.add("youtube")
     }
 
 
 })
 
+btnZptop.addEventListener("click", () => {
+    if (tvOnOf) {
+        zapping++
+        console.log(zapping);
+        screen.classList.remove(screen.classList[screen.classList.lenght - 1])
+        screen.classList.add("canal" + zapping)
+    }
+}
+)
+
 const switchTv = () => {
     // if (tvOnOf) {
-    //     tvOnOf = false 
+    //     tvOnOf = false
     //     screen.classList.remove(screen.classList[screen.classList.length - 1])
     //     screen.classList.add("basicScreen")
     //     buttonOnOff.style.backgroundColor = "black"
@@ -62,3 +82,4 @@ arrayButtons.map(
         })
     }
 )
+
